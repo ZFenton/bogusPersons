954ddd23b8ea736422c8ce5f28d72b549b8522d5
Zach Fenton <zfenton@selectviewdata.com>
2018-06-04 08:41:12 -0500

    reverse names added

--- BogusPersons/Program.fs
+++ BogusPersons/Program.fs
@@ -2,6 +2,7 @@
 open System
 open Bogus
 
+
 [<CLIMutable>]
 [<StructuredFormatDisplay("{firstName} {lastName}'s email is {email}")>]
 type Person = {firstName:string; lastName:string; email:string}
@@ -12,11 +13,12 @@ let faker =
         .RuleFor( (fun p -> p.lastName), fun (f:Faker) -> f.Name.LastName() )
         .RuleFor( (fun p -> p.email), fun (f:Faker) -> f.Internet.Email() )
 
+    
 [<EntryPoint>]
 let main argv = 
 
 
-    let persons = 10
+    let persons = 1
     let personList = faker.Generate(persons) |> List.ofSeq
 
 
@@ -33,7 +35,7 @@ let main argv =
 
 
 
-    printfn "**************** PRINTING ALL PERSONS' With GMail ****************\n"
+    printfn "\n**************** PRINTING ALL PERSONS' With GMail ****************\n"
 
     let gmailDomain = "@gmail.com"
     let filterEmailOn emailDomain = 
@@ -45,7 +47,40 @@ let main argv =
     let printRecordString person = printfn "%s" person
     
     let printPersonsWithGmail = personsWithGmailList |> List.map personToString |> List.iter printRecordString
-    printPersonsWithGmail |> ignore
+    let printIfNotEmpty = 
+            match List.isEmpty personsWithGmailList with
+            | true -> printfn "\n -- No person has gmail as their email\n"
+            | _ -> printPersonsWithGmail
+
+    printIfNotEmpty |> ignore
+
     printfn "\n******************************************************************\n"
 
+
+
+    printfn "\n**************** PRINTING ALL PERSONS' NAMES REVERSED ****************\n"
+
+    let reverseString (x : string) = String (Array.rev (x.ToCharArray()))
+    let reversedFirstName = fun person -> reverseString person.firstName
+
+    let printReversedFirstNames = personList |> List.map reversedFirstName |> List.iteri formatNamesWithIndex
+    printReversedFirstNames |> ignore
+    printfn "\n**********************************************************************\n"
+
+
+
+    printfn "\n**************** PRINTING LETTERS FROM NAMES AS THEY APPEAR ****************\n"
+
+    let lettersInNamesAsAppeared : char array = Array.empty
+    let firstNamesArray = personList |> List.map firstNameOnly
+
+    // Go through all indexes in list/array
+    // "add" the name from the first index to letter list/array (or build string?)
+    // for every proceeding name, check it's letters against the letters in the name from the first index
+    //        if the letter is not in the first name pulled, add the letter to the new string/array/list
+    // return value will be a list/array/ new string containing all the letters that appeared in the names
+
+    printfn "\n****************************************************************************\n"
+
+    
     0
