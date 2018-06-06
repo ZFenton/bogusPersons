46ab27a968b73f3cc090e7c65fd6f8f7ba8291b0
Zach Fenton <zfenton@selectviewdata.com>
2018-06-06 17:09:33 -0500

    function that output string that contains all unique first occurencs of letters from a list of strings.

--- BogusPersons/Program.fs
+++ BogusPersons/Program.fs
@@ -2,9 +2,8 @@
 open System
 open Bogus
 
-
 [<CLIMutable>]
-[<StructuredFormatDisplay("{firstName} {lastName}'s email is {email}")>]
+[<StructuredFormatDisplay("     {firstName} {lastName}'s email is {email}")>]
 type Person = {firstName:string; lastName:string; email:string}
 
 let faker =
@@ -17,25 +16,24 @@ let faker =
 [<EntryPoint>]
 let main argv = 
 
-
-    let persons = 1
+    let persons = 1000
     let personList = faker.Generate(persons) |> List.ofSeq
+    let firstNameOnly (person:Person) = person.firstName     // QUESTION: how does compiler know this is parameter is "person -> string" when not explicitly specified?
+    let personsFirstNames = personList |> List.map firstNameOnly
+
+    printfn "******************************************************************************************************************"
+    printfn "*************************************** PRINTING ALL PERSONS' FIRST NAMES ****************************************\n"
+
+    let formatNamesWithIndex = fun index firstName -> printfn "     Person %d) %s" (index + 1) firstName
 
 
-    printfn "\n*************** PRINTING ALL PERSONS' FIRST NAMES ****************\n"
-
-    // QUESTION: how does compiler know this is parameter is "person -> string" when not explicitly specified?
-    let firstNameOnly (person:Person) = person.firstName
-    let formatNamesWithIndex = fun index firstName -> printfn "Person %d) %s" (index + 1) firstName
-
-
-    let printPersonNames = personList |> List.map firstNameOnly |> List.iteri formatNamesWithIndex
+    let printPersonNames = personsFirstNames |> List.iteri formatNamesWithIndex
     printPersonNames |> ignore
-    printfn "\n******************************************************************\n"
+    printfn "\n******************************************************************************************************************"
 
 
 
-    printfn "\n**************** PRINTING ALL PERSONS' With GMail ****************\n"
+    printfn "**************************************** PRINTING ALL PERSONS' With GMail ****************************************\n"
 
     let gmailDomain = "@gmail.com"
     let filterEmailOn emailDomain = 
@@ -46,41 +44,63 @@ let main argv =
     let personToString (person:Person) = person.ToString()
     let printRecordString person = printfn "%s" person
     
-    let printPersonsWithGmail = personsWithGmailList |> List.map personToString |> List.iter printRecordString
+    let printPersonsWithGmail = 
+                                printfn "   There are %d out of %d persons with gmail as their email:\n" personsWithGmailList.Length personList.Length
+                                personsWithGmailList |> List.map personToString |> List.iter printRecordString
     let printIfNotEmpty = 
             match List.isEmpty personsWithGmailList with
-            | true -> printfn "\n -- No person has gmail as their email\n"
+            | true -> printfn "\n   -- No person has gmail as their email\n"
             | _ -> printPersonsWithGmail
 
     printIfNotEmpty |> ignore
 
-    printfn "\n******************************************************************\n"
+    printfn "\n******************************************************************************************************************"
 
 
 
-    printfn "\n**************** PRINTING ALL PERSONS' NAMES REVERSED ****************\n"
+    printfn "************************************** PRINTING ALL PERSONS' NAMES REVERSED **************************************\n"
 
     let reverseString (x : string) = String (Array.rev (x.ToCharArray()))
     let reversedFirstName = fun person -> reverseString person.firstName
 
     let printReversedFirstNames = personList |> List.map reversedFirstName |> List.iteri formatNamesWithIndex
     printReversedFirstNames |> ignore
-    printfn "\n**********************************************************************\n"
+    printfn "\n******************************************************************************************************************"
 
 
 
-    printfn "\n**************** PRINTING LETTERS FROM NAMES AS THEY APPEAR ****************\n"
+    printfn "*********************************** PRINTING LETTERS FROM NAMES AS THEY APPEAR ***********************************\n"
 
-    let lettersInNamesAsAppeared : char array = Array.empty
-    let firstNamesArray = personList |> List.map firstNameOnly
+    let distinctCharsOf stringList = stringList |> String.concat ""
+                                                |> Seq.distinct
+                                                |> Array.ofSeq
+                                                |> System.String
+    printfn "   All names with duplicate letters removed: %s" (distinctCharsOf personsFirstNames)
 
-    // Go through all indexes in list/array
-    // "add" the name from the first index to letter list/array (or build string?)
-    // for every proceeding name, check it's letters against the letters in the name from the first index
-    //        if the letter is not in the first name pulled, add the letter to the new string/array/list
-    // return value will be a list/array/ new string containing all the letters that appeared in the names
+    ////         OLD VERBOSE SOLUTION BY USING THE KEYWORD MUTABLE
+    //let lower = ['a' .. 'z']
+    //let upper = ['A' .. 'Z']
+    //let mutable concatedNames = ["John"; "James"; "Jack"] |> List.fold (+) ""
 
-    printfn "\n****************************************************************************\n"
+    //let greaterThanOne (length : int) = length > 1
+    //let stripChars (str : string) letter =
+    //    let parts = str.Split([| letter |])
+    //    match greaterThanOne (Array.length parts) with
+    //    | true -> seq {
+    //                    yield Array.head parts
+    //                    yield string letter
+    //                    yield! Array.tail parts
+    //                  }
+    //                  |> String.concat ""
+    //    | _ -> str
+    
+    //let stripDuplicateLetter = fun letter -> concatedNames <- (stripChars concatedNames letter)
+    //let stripAllButFirst list = list |> List.iter stripDuplicateLetter
+    //stripAllButFirst lower
+    //stripAllButFirst upper
+    //printfn "All names with duplicate letters removed: %s" concatedNames
+
+    printfn "\n******************************************************************************************************************\n"
 
     
     0
